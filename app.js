654b8b2e0bf5314d72cf8986145fbfd8fdae7943
ulascan54 <ulascandemirbag@gmail.com>
2021-09-24 18:58:06 +0300

    3d obje oluşturuldu

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-
-
 const container = document.getElementById('container')
 
-let scene = new THREE.Scene()
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(80,window.innerWidth / window.innerHeight,0.1,1000)
+camera.position.set(3,0,-3);
+camera.lookAt(0,0,0);
 
-let camera = new THREE.PerspectiveCamera(80 , window.innerWidth / window.innerHeight,0.1,1000)
-
-let  renderer = new THREE.WebGL1Renderer()
-
+const  renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth,window.innerHeight)
-
 container.appendChild(renderer.domElement)
+
+const planeGeometry=new THREE.PlaneGeometry(5,3,5,3)
+const flagMat= new THREE.MeshBasicMaterial({color:'white',wireframe:true})
+const flag =new THREE.Mesh(planeGeometry,flagMat)
+
+scene.add(flag)
+
+function animate(){
+    renderer.render(scene,camera)
+    requestAnimationFrame(animate)
+}
+animate()
