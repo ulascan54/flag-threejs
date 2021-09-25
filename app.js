e4504725f5fb0aa2ba4132e21d94920c89d13c35
ulascan54 <ulascandemirbag@gmail.com>
2021-09-25 14:24:48 +0300

    türk bağrağının dalga hareketine eklenmesi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const container = document.getElementById('container')
 
 var scene = new THREE.Scene()
 var camera = new THREE.PerspectiveCamera(80,window.innerWidth / window.innerHeight,0.1,1000)
-
+const loader = new THREE.TextureLoader()
 
 var  renderer = new THREE.WebGLRenderer({
     alpha:true,
@@ -12,7 +12,7 @@ renderer.setSize(window.innerWidth,window.innerHeight)
 container.appendChild(renderer.domElement)
 
 var planeGeometry=new THREE.PlaneGeometry(5,3,200,120)
-var flagMat= new THREE.MeshBasicMaterial({color:'red', wireframe:true})
+var flagMat= new THREE.MeshBasicMaterial({map: loader.load("t.jpeg")})
 var flag =new THREE.Mesh(planeGeometry,flagMat)
 
 scene.add(flag)
@@ -26,7 +26,11 @@ function animate(){
     const time = clock.getElapsedTime()
 
     flag.geometry.vertices.map(v=>{
-        v.z = 0.5 * Math.sin(v.x*2+time)
+        const wavex1=0.5 * Math.sin(v.x*2+time)
+        const wavex2=0.25 * Math.sin(v.x*3+time*2)
+        const wavex3=0.1 * Math.sin(v.y+time)
+
+        v.z = wavex1+wavex2+wavex3
     })
     flag.geometry.verticesNeedUpdate=true
 
